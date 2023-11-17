319947e22beb501a5b2915b63a1303cf7061dd6b
Tanuja Subhash Shinde <118378357+TanujaShinde29@users.noreply.github.com>
2023-11-17 13:28:27 +0530

    Add files via upload

---
+++ diabetes prediction web app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 12:37:51 2023
+
+@author: shind
+"""
+
+import numpy as np
+import pickle
+import streamlit
+
+loaded_model = pickle.load(open('C:/Users/shind/Desktop/ML Project/diabetes_model.sav', 'rb'))
